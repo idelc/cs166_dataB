c1f1124ac33a45a71684dd6cc047151a9a0b6f12
idelc <43787922+idelc@users.noreply.github.com>
2022-03-07 08:45:40 -0800

    undo

--- ProfNetwork.java
+++ ProfNetwork.java
@@ -230,13 +230,13 @@ public class ProfNetwork {
          System.err.println (
             "Usage: " +
             "java [-classpath <classpath>] " +
-            Messenger.class.getName () +
+            ProfNetwork.class.getName () +
             " <dbname> <port> <user>");
          return;
       }//end if
 
       Greeting();
-      Messenger esql = null;
+      ProfNetwork esql = null;
       try{
          // use postgres JDBC driver.
          Class.forName ("org.postgresql.Driver").newInstance ();
@@ -245,7 +245,7 @@ public class ProfNetwork {
          String dbname = args[0];
          String dbport = args[1];
          String user = args[2];
-         esql = new Messenger (dbname, dbport, user, "");
+         esql = new ProfNetwork (dbname, dbport, user, "");
 
          boolean keepon = true;
          while(keepon) {
@@ -267,10 +267,11 @@ public class ProfNetwork {
               while(usermenu) {
                 System.out.println("MAIN MENU");
                 System.out.println("---------");
-                System.out.println("1. Goto Friend List");
+                System.out.println("1. Go to Friend List");
                 System.out.println("2. Update Profile");
                 System.out.println("3. Write a new message");
                 System.out.println("4. Send Friend Request");
+                System.out.println("5. View messages");
                 System.out.println(".........................");
                 System.out.println("9. Log out");
                 switch (readChoice()){
@@ -278,6 +279,7 @@ public class ProfNetwork {
                    case 2: UpdateProfile(esql); break;
                    case 3: NewMessage(esql); break;
                    case 4: SendRequest(esql); break;
+                   case 5: ViewMessage(esql); break;
                    case 9: usermenu = false; break;
                    default : System.out.println("Unrecognized choice!"); break;
                 }
@@ -331,7 +333,7 @@ public class ProfNetwork {
     * Creates a new user with privided login, passowrd and phoneNum
     * An empty block and contact list would be generated and associated with a user
     **/
-   public static void CreateUser(Messenger esql){
+   public static void CreateUser(ProfNetwork esql){
       try{
          System.out.print("\tEnter user login: ");
          String login = in.readLine();
@@ -354,7 +356,7 @@ public class ProfNetwork {
     * Check log in credentials for an existing user
     * @return User login or null is the user does not exist
     **/
-   public static String LogIn(Messenger esql){
+   public static String LogIn(ProfNetwork esql){
       try{
          System.out.print("\tEnter user login: ");
          String login = in.readLine();
@@ -373,5 +375,103 @@ public class ProfNetwork {
    }//end
 
 // Rest of the functions definition go in here
+   /*
+   * View friends and can access friends profile. Additionally you can send a connection request or a message to them 
+   */
+    public static void FriendList(ProfNetwork esql){
+        
+    }
 
+    /*
+    * Updating user's password
+    */
+    public static void UpdateProfile(ProfNetwork esql){
+        try{
+         System.out.print("\tEnter user login: ");
+         String login = in.readLine();
+         System.out.print("\tEnter new password: ");
+         String newPassword = in.readLine();
+         String query = String.format("SELECT * FROM USR WHERE userId = '%s' AND password = '%s'", login, newPassword);
+         int userNum = esql.executeQuery(query);
+         if (userNum > 0)
+                return login; // THIS NEEDS TO CHANGE
+         return null;
+        }catch(Exception e){
+         System.err.println (e.getMessage ());
+         return null;
+      }
+    }
+
+    /*
+    * Send message to anyone on network 
+    */
+    public static void NewMessage(ProfNetwork esql){
+        try{
+            System.out.print("\tEnter user's id: ");
+            String senderId = in.readLine();
+            System.out.print("\tEnter receiverId: ");
+            String receiverId = in.readLine();
+            System.out.print("\tEnter message you want to send to receiver: ");
+            String contents = in.readLine();
+            String query = String.format("INSERT INTO MESSAGE (userId, password, email, contact_list) VALUES ('%s','%s','%s')", senderId, receiverId, contents);
+            int userNum = esql.executeQuery(query);
+            if (userNum > 0){
+                return login;
+            }
+            return null;
+        }catch(Exception e){
+         System.err.println (e.getMessage ());
+         return null;
+      }
+    }
+
+    /* Send request for connection  */
+    public static void SendRequest(ProfNetwork esql){
+        
+    }
+    
+    /* View user's messages and have the option to delete them*/
+    public static void ViewMessage(ProfNetwork esql){
+        try{
+            System.out.print("\tEnter user login: ");
+            String receiverId = in.readLine();
+            String query = String.format("SELECT * FROM MESSAGE WHERE receiverId = '%s'", receiverId);
+            int userNum = esql.executeQuery(query);
+            if (userNum > 0){
+                return login;
+            }
+            System.out.println("1. Delete a specific message from a user to Friend List");
+            System.out.println("2. Go back");
+            switch (readChoice()){
+               case 1: DeleteMessage(esql); break;
+               case 2: break; // NOT SURE IF THIS IS RIGHT
+            return null;
+        }catch(Exception e){
+         System.err.println (e.getMessage ());
+         return null;
+      }
+    }
+    
+    /* Delete a message */
+    public static void ViewMessage(ProfNetwork esql){
+        try{
+            System.out.print("\tEnter msgId of the message you want to delete: ");
+            String receiverId = in.readLine();
+            String query = String.format("DELETE FROM MESSAGE WHERE receiverId = '%s'", receiverId);
+            int userNum = esql.executeQuery(query);
+            if (userNum > 0){
+                return login;
+            }
+            return null;
+        }catch(Exception e){
+         System.err.println (e.getMessage ());
+         return null;
+      }
+    }
+    
+    /* Accept or decline a request for connection  */
+    public static void SendRequest(ProfNetwork esql){
+        
+    }
+    /* */
 }//end ProfNetwork
