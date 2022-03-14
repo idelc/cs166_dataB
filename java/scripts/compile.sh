f6f33a717f8ec853ff68af20df3eb63c5d064908
Ivann De la Cruz Andrade <idel012@wch132-18.cs.ucr.edu>
2022-03-13 20:51:30 -0700

    Final File Formating

--- java/scripts/compile.sh
+++ java/scripts/compile.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+export DB_NAME=$USER"_DB"
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]}" )" && pwd)"
 # Indicate the path of the java compiler to use
 export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Export classpath with the postgressql driver
-export CLASSPATH=$CLASSPATH:$PWD/pg73jdbc3.jar
+export CLASSPATH=$CLASSPATH:$PWD/../lib/pg73jdbc3.jar
 
 # compile the java program
-javac ProfNetwork.java
+javac -d $DIR/../classes $DIR/../src/ProfNetwork.java
 
 #run the java program
 #Use your database name, port number and login
-java ProfNetwork $USER"_DB" $PGPORT $USER
+java ProfNetwork $DB_NAME $PGPORT $USER
+
 
